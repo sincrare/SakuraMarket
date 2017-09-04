fbe43df8bb5590efb404459f40f22a82a7997841
SINCRARE <SINCRARE@KEIGO-MAC.local>
2017-09-04 23:45:42 +0900

    Refactoring

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2147483647 }
   include RankedModel
   ranks :row_order
   mount_uploader :image, ImageUploader
